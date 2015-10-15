d58cedcea42ccfb695e33c24ccdc095cb47d4db2
Nguyen Thanh Duc <DucNguyen@techmasters-mac-pro.local>
2015-10-15 11:34:11 +0700

    Initial Commit

---
+++ iWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iWeather
+//
+//  Created by Nguyen Thanh Duc on 10/15/15.
+//  Copyright © 2015 Duc Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iWeather/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iWeather
+//
+//  Created by Nguyen Thanh Duc on 10/15/15.
+//  Copyright © 2015 Duc Nguyen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
