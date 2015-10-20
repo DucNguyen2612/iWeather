b8b667fcdca770e24a76107ee483b677039b5988
Nguyen Thanh Duc <DucNguyen@Techmasters-Mac-Pro.local>
2015-10-20 15:20:48 +0700

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+iWeather written by Nguyen Thanh Duc
+for iOS class 34
+MIT license  
+
--- iWeather/ViewController.m
+++ iWeather/ViewController.m
@@ -7,21 +7,68 @@
 //
 
 #import "ViewController.h"
-
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *location;
+@property (weak, nonatomic) IBOutlet UIButton *temperature;
+@property (weak, nonatomic) IBOutlet UIImageView *weatherIcon;
+@property (weak, nonatomic) IBOutlet UILabel *quote;
+@property (weak, nonatomic) IBOutlet UILabel *celcius;
+
 
 @end
 
 @implementation ViewController
+{
+    NSArray* quotes;
+    NSArray* locations;
+    NSArray* photoFiles;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    quotes = @[@"Di mot ngay dang hoc mot sang khon", @"Uong nuoc nho nguon", @"Cong cha nhu nui thai son", @"Nghia me nhu nuoc trong nguon chay ra",@"Sinh ra ngheo khong co toi Chet vi ngheo moi la toi"];
+    
+    locations = @[@"Hai Ba Trung, HaNoi", @"Xin Feng, Taiwan",@"New York, USA"];
+    
+    photoFiles = @[@"windy", @"thunder", @"sunny", @"rain"];
+    
+    _celcius.text = @"C";
+   
+
+}
+- (IBAction)updateWeather:(id)sender {
+    int locationIndex = arc4random_uniform((int)locations.count);
+    self.location.text = locations[locationIndex];
+    int quoteIndex = arc4random_uniform((int)quotes.count);
+    self.quote.text = quotes[quoteIndex];
+    int photoIndex = arc4random_uniform((int)photoFiles.count);
+    self.weatherIcon.image = [UIImage imageNamed:(photoFiles[photoIndex])];
+    NSString* string = [NSString stringWithFormat:@"%2.1f",[self getTemperature]];
+    [self.temperature setTitle: string forState: UIControlStateNormal];
+}
+- (float) getTemperature{
+    return 15.0 + arc4random_uniform(16) + (float)arc4random() /(float) INT32_MAX;
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
+- (IBAction)Taptemperature:(id)sender
+{
+    if ([_celcius.text  isEqual: @"C"]) {
+        _celcius.text = @"F";
+    }
+    else {
+        _celcius.text = @"C";
+    }
+    
+    NSString* string1 = [NSString stringWithFormat:@"%2.1f",[self touchTemperature]];
+    [self.temperature setTitle: string1 forState:UIControlStateNormal];
+
+}
+- (float) touchTemperature
+{
+        return 20.0 + arc4random_uniform(16) + (float)arc4random() /(float) INT32_MAX;
+    
+}
 @end
